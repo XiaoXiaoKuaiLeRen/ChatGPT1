aa95542dd5f18629abe1df799fb574608b323da1
XiaoXiaoKuaiLeRen <1485350913@outlook.com>
2023-04-24 20:42:13 +0800

    6666

--- app/constant.ts
+++ app/constant.ts
@@ -1,12 +1,12 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const OWNER = "doc/p";
+export const REPO =
+  "b02c3d2f7ffe9db2c121af33b1b15bd665725afb?u=ea33c42eea634f2c9cf0533cd3a50bdc";
+export const REPO_URL = `https://docs.qq.com/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://docs.qq.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
-
 export enum Path {
   Home = "/",
   Chat = "/chat",
