b586c25e12e32364fc2709d47b1aab04c6727c6d
XiaoXiaoKuaiLeRen <1485350913@outlook.com>
2023-04-25 18:56:48 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/config";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请点击左下角设置按钮输入访问密码。",
+    Unauthorized: "现在是未授权状态，请在公众号KKGPT666获取今日授权码。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
@@ -154,7 +154,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "想要联系我的，左下角小猫或者公众号直接联系",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) =>
