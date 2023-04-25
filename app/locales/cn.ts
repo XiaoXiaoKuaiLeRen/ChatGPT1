fa150af54df85972cfabf07f4d7ed2e15e1b707f
XiaoXiaoKuaiLeRen <1485350913@outlook.com>
2023-04-25 19:04:05 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/config";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请在公众号KKGPT666获取今日授权码。",
+    Unauthorized: "现在是未授权状态，请在公众号KKGPT666回复授权码获取今日授权码。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
