3cc73572c3483dca43d3ca570c04f8484006efae
unknown <1485350913@outlook.com>
2023-05-11 20:43:17 +0800

    345
    34534

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,7 +96,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>KK ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
           KK的AI小助手.
         </div>
